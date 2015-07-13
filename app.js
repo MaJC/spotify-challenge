3af4ab5ce1b3cb814be6b1332e904cdca4e11b00
CharlesMa <charlesma95@gmail.com>
2015-07-13 10:06:53 +0800

    Added and finished index, css, javascript

--- app.js
+++ app.js
@@ -1,15 +1,84 @@
-var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
-var myApp = angular.module('myApp', [])
 
-var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
-  $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
+
+var data;
+var baseUrl = 'https://api.spotify.com/v1/search?q=' ;
+var myApp = angular.module('myApp', [])
+var typesearch;
+
+
+var myCtrl = myApp.controller('myCtrl', function($scope, $http, $window) {
+  $scope.audioObject = {};
+
+  $scope.feed = {};
+
+  $scope.searchtypes = [
+    {
+      'name': 'Artist',
+      'value': 'artists',
+      'id': 'artist'
+    },
+    {
+      'name': 'Track',
+      'value': 'tracks',
+      'id': 'track'
+    },
+    {
+      'name': 'Album',
+      'value': 'albums',
+      'id': 'album'
+    },
+  ];
+
+  $scope.typechange = function() {
+    $('#searchsong').show();
+  }
+ 
+  $scope.tracklistshow = false;
+  $scope.toplistshow = false;
+  $scope.testtype = '';
+
+  $scope.getSearch = function() {
+    $scope.testtype = $scope.feed.searchtype.id;
+    $scope.tracklistshow = false;
+    $scope.toplistshow = false;
+   
+    $http.get(baseUrl + $scope.track + "&type=" + $scope.testtype).success(function(response){
+      if ($scope.testtype == "track") {
+        $scope.data = $scope.chosentype = response.tracks.items;
+      } else if ($scope.testtype == "album") {
+        $scope.data = $scope.chosentype = response.albums.items;
+      } else if ($scope.testtype == "artist") {
+        $scope.data = $scope.chosentype = response.artists.items;
+      } 
+            
     })
   }
+
+  var albumurl = 'https://api.spotify.com/v1/albums/';
+  $scope.currentId = "";
+ 
+  $scope.showalbumtracks = function(id) {
+    $scope.tracklistshow = true;
+    $http.get(albumurl + id + "/tracks").success(function(response){
+      $scope.currentId = id;
+      $scope.albumtracks = response.items;
+     
+    })
+  }
+
+  var toptracksurl = 'https://api.spotify.com/v1/artists/';
+  $scope.artistId = "";
+ 
+  $scope.showtoptracks = function(id) {
+    $scope.toplistshow = true;
+    $http.get(toptracksurl + id + "/top-tracks?country=US").success(function(response){
+      $scope.artistId = id;
+      $scope.toptracks = response.tracks;
+    });
+    
+  }
+
+
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
@@ -29,3 +98,5 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
 $('body').tooltip({
     selector: '[title]'
 });
+
+
